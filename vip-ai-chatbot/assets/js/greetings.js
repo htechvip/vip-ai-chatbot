8b1742bb4801d9bd431bd330f0bfc6316a4a5bbd
cho-nan tsai <sunnybook8@gmail.com>
2023-10-04 17:22:00 -0700

    added zip file

--- vip-ai-chatbot/assets/js/greetings.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function ($) {
-    localStorage.setItem('chatgpt_initial_greeting', greetings_data.initial_greeting);
-    localStorage.setItem('chatgpt_subsequent_greeting', greetings_data.subsequent_greeting);
-});
